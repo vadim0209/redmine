8fbd2a456ecc650b56b0a9913bec5f4e6bbc5952
vadim0209 <84313402+vadim0209@users.noreply.github.com>
2021-05-25 08:16:55 -0700

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag nginx vadim0209/redmine:latest
-docker push vadim0209/redmine
-docker tag mariadb vadim0209/redmine:latest
-docker push vadim0209/redmine
+docker tag nginx vadim0209/nginx:latest
+docker push vadim0209/nginx
+docker tag mariadb vadim0209/mariadb:latest
+docker push vadim0209/mariadb
 docker tag redmine vadim0209/redmine:latest
 docker push vadim0209/redmine
-
