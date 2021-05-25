15efee64b34944b7d037b8142add41478c7e261e
vadim0209 <84313402+vadim0209@users.noreply.github.com>
2021-05-25 06:44:04 -0700

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME"
 docker tag redmine vadim0209/redmine
 docker push vadim0209/redmine
