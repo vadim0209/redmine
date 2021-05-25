f630a4f81c7024f8e90547e9d86647cbeee5e6dd
Vadim Alekseev <vadimalekseev8991@gmail.com>
2021-05-25 08:11:48 -0700

    redmine

--- push.sh
+++ push.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker tag nginx vadim0209/redmine:latest
+docker push vadim0209/redmine
+docker tag mariadb vadim0209/redmine:latest
+docker push vadim0209/redmine
 docker tag redmine vadim0209/redmine:latest
 docker push vadim0209/redmine
+
